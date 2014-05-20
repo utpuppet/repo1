c3efb3eaab8607aa44ba36b6fb7c3ea25aad4b57
ut <youting@c-platform.com>
2014-05-20 14:05:17 +0800

    stylish

--- modules/puppet/manifests/init.pp
+++ modules/puppet/manifests/init.pp
@@ -1,19 +1,19 @@
 class puppet {
-	file { '/usr/local/bin/papply':
-		source => 'puppet:///modules/puppet/papply.sh',
-		mode => '0755',
-	}
+  file { '/usr/local/bin/papply':
+    source => 'puppet:///modules/puppet/papply.sh',
+    mode => '0755',
+  }
 
-	file { '/usr/local/bin/pull-updates':
-		source => 'puppet:///modules/puppet/pull-updates.sh',
-		mode => '0755',
-	}
+  file { '/usr/local/bin/pull-updates':
+    source => 'puppet:///modules/puppet/pull-updates.sh',
+    mode => '0755',
+  }
 
-	cron { 'run-puppet':
-		ensure => present,
-		user => 'root',
-		command => '/usr/local/bin/pull-updates',
-		minute => '*/10',
-		hour => '*',
-	}
+  cron { 'run-puppet':
+    ensure => present,
+    user => 'root',
+    command => '/usr/local/bin/pull-updates',
+    minute => '*/10',
+    hour => '*',
+  }
 }
